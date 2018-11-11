c863dfc00faabb438afeba110b299f54de2aa082
Andrés Marenco <andresmarenco@gmail.com>
2018-11-11 06:17:30 -0600

    Fixed NPE on empty security context

--- apollo-core/src/main/java/com/apolloframework/security/AuthUtils.java
+++ apollo-core/src/main/java/com/apolloframework/security/AuthUtils.java
@@ -1,14 +1,20 @@
 package com.apolloframework.security;
 
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuthUtils {
     
     /**
      * Gets the user from the current security context
-     * @return the current user
+     * @return the current user or <code>null</code>
      */
     public static AuthUser getCurrentUser() {
-        return (AuthUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SecurityContext context = SecurityContextHolder.getContext();
+        if(context != null && context.getAuthentication() != null) {
+            return (AuthUser)context.getAuthentication().getPrincipal();
+        } else {
+            return null;
+        }
     }
 }
